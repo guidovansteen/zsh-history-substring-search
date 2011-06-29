09b686e865d5d24d1c8bc94c64b95ec47e73dd96
Guido <guido.vansteen@gmail.com>
2011-06-29 03:20:38 -0700

    Align with Sunaku's repo

--- history-substring-search.zsh
+++ history-substring-search.zsh
@@ -1,26 +1,94 @@
 #!/usr/bin/env zsh
 #
 # This is a clean-room implementation of the Fish[1] shell's history search
-# feature, where you can enter any part of any previous command and press
-# the UP and DOWN arrow keys to cycle through all matching commands.
+# feature, where you can type in any part of any previously entered command
+# and press the UP and DOWN arrow keys to cycle through the matching commands.
 #
-# This was originally implemented by Peter Stephenson[2], who published it to
-# the ZSH users mailing list (thereby making it public domain) in September
+#-----------------------------------------------------------------------------
+# Usage
+#-----------------------------------------------------------------------------
+#
+# 1. Load this script into your interactive ZSH session:
+#
+#       % source history-substring-search.zsh
+#
+#    If you want to use the zsh-syntax-highlighting[6] script along with this
+#    script, then make sure that you load it *before* you load this script:
+#
+#       % source zsh-syntax-highlighting.zsh
+#       % source history-substring-search.zsh
+#
+# 2. Type any part of any previous command and then:
+#
+#     * Press the UP arrow key to select the nearest command that (1) contains
+#       your query and (2) is older than the current command in the command
+#       history.
+#
+#     * Press the DOWN arrow key to select the nearest command that (1)
+#       contains your query and (2) is newer than the current command in the
+#       command history.
+#
+#     * Press ^U (the Control and U keys simultaneously) to abort the search.
+#
+# 3. If a matching command spans more than one line of text, press the LEFT
+#    arrow key to move the cursor away from the end of the command, and then:
+#
+#     * Press the UP arrow key to move the cursor to the line above.  When the
+#       cursor reaches the first line of the command, pressing the UP arrow
+#       key again will cause this script to perform another search.
+#
+#     * Press the DOWN arrow key to move the cursor to the line below.  When
+#       the cursor reaches the last line of the command, pressing the DOWN
+#       arrow key again will cause this script to perform another search.
+#
+#-----------------------------------------------------------------------------
+# Configuration
+#-----------------------------------------------------------------------------
+#
+# This script defines the following global variables. You may override their
+# default values only after having loaded this script into your ZSH session.
+#
+# * HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND is a global variable that defines
+#   how the query should be highlighted inside a matching command. Its default
+#   value causes this script to highlight using bold, white text on a magenta
+#   background. See the "Character Highlighting" section in the zshzle(1) man
+#   page to learn about the kinds of values you may assign to this variable.
+#
+# * HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND is a global variable that
+#   defines how the query should be highlighted when no commands in the
+#   history match it. Its default value causes this script to highlight using
+#   bold, white text on a red background. See the "Character Highlighting"
+#   section in the zshzle(1) man page to learn about the kinds of values you
+#   may assign to this variable.
+#
+# * HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS is a global variable that defines
+#   how the command history will be searched for your query. Its default value
+#   causes this script to perform a case-insensitive search. See the "Globbing
+#   Flags" section in the zshexpn(1) man page to learn about the kinds of
+#   values you may assign to this variable.
+#
+#-----------------------------------------------------------------------------
+# History
+#-----------------------------------------------------------------------------
+#
+# This script was originally written by Peter Stephenson[2], who published it
+# to the ZSH users mailing list (thereby making it public domain) in September
 # 2009. It was later revised by Guido van Steen and released under the BSD
 # license (see below) as part of the fizsh[3] project in January 2011.
 #
-# This was later extracted from fizsh[3] release 1.0.1, refactored heavily,
-# and repackaged as an OH MY ZSHELL plugin[4] and as an independently loadable
+# It was later extracted from fizsh[3] release 1.0.1, refactored heavily, and
+# repackaged as both an oh-my-zsh plugin[4] and as an independently loadable
 # ZSH script[5] by Suraj N. Kurapati in 2011.
 #
-# Further improvements were contributed by Guido van Steen, Sorin Ionescu and
-# Vincent Guerci.
+# It was further developed[4] by Guido van Steen, Suraj N. Kurapati, Sorin
+# Ionescu, and Vincent Guerci in 2011.
 #
-# [1] http://fishshell.com
-# [2] http://www.zsh.org/mla/users/2009/msg00818.html
-# [3] http://sourceforge.net/projects/fizsh/
-# [4] https://github.com/robbyrussell/oh-my-zsh/pull/215
-# [5] https://github.com/sunaku/zsh-history-substring-search
+# [1]: http://fishshell.com
+# [2]: http://www.zsh.org/mla/users/2009/msg00818.html
+# [3]: http://sourceforge.net/projects/fizsh/
+# [4]: https://github.com/robbyrussell/oh-my-zsh/pull/215
+# [5]: https://github.com/sunaku/zsh-history-substring-search
+# [6]: https://github.com/nicoulaj/zsh-syntax-highlighting
 #
 ##############################################################################
 #
@@ -66,13 +134,13 @@
 
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=white,bold'
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
-HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i' # "i" means case insensitive, see "Globbing Flags" in zshexpn(1)
+HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
 
 #-----------------------------------------------------------------------------
 # the main ZLE widgets
 #-----------------------------------------------------------------------------
 
-history-substring-search-up() {
+function history-substring-search-up() {
   _history-substring-search-begin
 
   _history-substring-search-up-history ||
@@ -82,7 +150,7 @@ history-substring-search-up() {
   _history-substring-search-end
 }
 
-history-substring-search-down() {
+function history-substring-search-down() {
   _history-substring-search-begin
 
   _history-substring-search-down-history ||
@@ -125,7 +193,7 @@ if [[ $+functions[_zsh_highlight] -eq 0 ]]; then
   # inserts printable characters into $BUFFER
   #
   function ordinary-key-press() {
-    if [[ $KEYS = [[:print:]] ]]; then
+    if [[ $KEYS == [[:print:]] ]]; then
       region_highlight=()
     fi
     zle .self-insert
@@ -210,8 +278,9 @@ if [[ $+functions[_zsh_highlight] -eq 0 ]]; then
   #-------------->8------------------->8------------------->8-----------------
 fi
 
-_history-substring-search-begin() {
+function _history-substring-search-begin() {
   _history_substring_search_move_cursor_eol=false
+  _history_substring_search_query_highlight=
 
   #
   # Continue using the previous $_history_substring_search_result by default,
@@ -257,12 +326,28 @@ _history-substring-search-begin() {
   fi
 }
 
-_history-substring-search-end() {
+function _history-substring-search-end() {
   _history_substring_search_result=$BUFFER
 
+  # move the cursor to the end of the command line
   if [[ $_history_substring_search_move_cursor_eol == true ]]; then
-    # Move the cursor to the end of the $BUFFER.
     CURSOR=${#BUFFER}
+  fi
+
+  # highlight command line using zsh-syntax-highlighting
+  _zsh_highlight
+
+  # highlight the search query inside the command line
+  if [[ -n $_history_substring_search_query_highlight && -n $_history_substring_search_query ]]; then
+    #
+    # The following expression yields a variable $MBEGIN, which
+    # indicates the begin position + 1 of the first occurrence
+    # of _history_substring_search_query_escaped in $BUFFER.
+    #
+    : ${(S)BUFFER##(#m$HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS)($_history_substring_search_query##)}
+    local begin=$(( MBEGIN - 1 ))
+    local end=$(( begin + $#_history_substring_search_query ))
+    region_highlight+=("$begin $end $_history_substring_search_query_highlight")
   fi
 
   # For debugging purposes:
@@ -273,23 +358,7 @@ _history-substring-search-end() {
   true
 }
 
-_history-substring-search-highlight() {
-  _zsh_highlight
-
-  if [[ -n $_history_substring_search_query ]]; then
-    #
-    # The following expression yields a variable $MBEGIN, which
-    # indicates the begin position + 1 of the first occurrence
-    # of _history_substring_search_query_escaped in $BUFFER.
-    #
-    : ${(S)BUFFER##(#m$HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS)($_history_substring_search_query##)}
-    local begin=$(( $MBEGIN - 1 ))
-    local end=$(( $begin + $#_history_substring_search_query ))
-    region_highlight+=("$begin $end $1")
-  fi
-}
-
-_history-substring-search-up-buffer() {
+function _history-substring-search-up-buffer() {
   #
   # Check if the UP arrow was pressed to move the cursor within a multi-line
   # buffer. This amounts to three tests:
@@ -318,7 +387,7 @@ _history-substring-search-up-buffer() {
   false
 }
 
-_history-substring-search-down-buffer() {
+function _history-substring-search-down-buffer() {
   #
   # Check if the DOWN arrow was pressed to move the cursor within a multi-line
   # buffer. This amounts to three tests:
@@ -347,38 +416,52 @@ _history-substring-search-down-buffer() {
   false
 }
 
-_history-substring-search-up-history() {
+function _history-substring-search-up-history() {
+  #
+  # Behave like up in ZSH, except clear the $BUFFER
+  # when beginning of history is reached like in Fish.
+  #
   if [[ -z $_history_substring_search_query ]]; then
-    if [[ $HISTNO -gt 1 ]]; then
-      zle up-history
+
+    # we have reached the absolute top of history
+    if [[ $HISTNO -eq 1 ]]; then
+      BUFFER=
+
+    # going up from somewhere below the top of history
     else
-      BUFFER=""
+      zle up-history
     fi
+
     return true
   fi
 
   false
 }
 
-_history-substring-search-down-history() {
+function _history-substring-search-down-history() {
+  #
+  # Behave like down-history in ZSH, except clear the
+  # $BUFFER when end of history is reached like in Fish.
+  #
   if [[ -z $_history_substring_search_query ]]; then
-    if [[ $HISTNO -eq 1 && $BUFFER == "" ]]; then
 
-      # down-history would go too far here, because we have not decreased $HISTNO
-      # after we emptied the buffer in _history-substring-search-up-history
-      # So instead we call:
-      BUFFER=$(history -n 1 1)
-      CURSOR=$#BUFFER
+    # going down from the absolute top of history
+    if [[ $HISTNO -eq 1 && -z $BUFFER ]]; then
+      BUFFER=${history[1]}
+      _history_substring_search_move_cursor_eol=true
+
+    # going down from somewhere above the bottom of history
     else
       zle down-history
     fi
+
     return true
   fi
 
   false
 }
 
-_history-substring-search-up-search() {
+function _history-substring-search-up-search() {
   _history_substring_search_move_cursor_eol=true
 
   #
@@ -411,7 +494,7 @@ _history-substring-search-up-search() {
     #
     (( _history_substring_search_match_index-- ))
     BUFFER=$history[$_history_substring_search_matches[$_history_substring_search_match_index]]
-    _history-substring-search-highlight $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
+    _history_substring_search_query_highlight=$HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
 
   elif [[ $_history_substring_search_match_index -eq 1 ]]; then
     #
@@ -431,7 +514,7 @@ _history-substring-search-up-search() {
     (( _history_substring_search_match_index-- ))
     _history_substring_search_old_buffer=$BUFFER
     BUFFER=$_history_substring_search_query
-    _history-substring-search-highlight $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
+    _history_substring_search_query_highlight=$HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
 
   elif [[ $_history_substring_search_match_index -eq $_history_substring_search_matches_count_plus ]]; then
     #
@@ -447,11 +530,11 @@ _history-substring-search-up-search() {
     #
     (( _history_substring_search_match_index-- ))
     BUFFER=$_history_substring_search_old_buffer
-    _history-substring-search-highlight $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
+    _history_substring_search_query_highlight=$HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
   fi
 }
 
-_history-substring-search-down-search() {
+function _history-substring-search-down-search() {
   _history_substring_search_move_cursor_eol=true
 
   #
@@ -482,7 +565,7 @@ _history-substring-search-down-search() {
     # 1. We have to use $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
     #    to highlight the current buffer.
     #
-    _history-substring-search-highlight $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
+    _history_substring_search_query_highlight=$HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
 
   elif [[ $_history_substring_search_match_index -le $_history_substring_search_matches_count_sans ]]; then
     #
@@ -495,7 +578,7 @@ _history-substring-search-down-search() {
     #
     (( _history_substring_search_match_index++ ))
     BUFFER=$history[$_history_substring_search_matches[$_history_substring_search_match_index]]
-    _history-substring-search-highlight $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
+    _history_substring_search_query_highlight=$HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
 
   elif [[ $_history_substring_search_match_index -eq $_history_substring_search_matches_count ]]; then
     #
@@ -515,7 +598,7 @@ _history-substring-search-down-search() {
     (( _history_substring_search_match_index++ ))
     _history_substring_search_old_buffer=$BUFFER
     BUFFER=$_history_substring_search_query
-    _history-substring-search-highlight $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
+    _history_substring_search_query_highlight=$HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
 
   elif [[ $_history_substring_search_match_index -eq 0 ]]; then
     #
@@ -531,7 +614,7 @@ _history-substring-search-down-search() {
     #
     (( _history_substring_search_match_index++ ))
     BUFFER=$_history_substring_search_old_buffer
-    _history-substring-search-highlight $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
+    _history_substring_search_query_highlight=$HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
   fi
 }
 
